400712f12aa214eaac71f69a1d584206127c5f22
Jaybles <jay.allen@actual-experience.com>
2022-05-10 15:02:13 +0100

    [amplify-test] I'm commiting! Don't know what but LETS GOOOOO

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nationalparksd5a54850": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s30109bdd8": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "nationalparks": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
